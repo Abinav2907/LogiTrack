6c5065b334f9f4bb413fb859e9f6f6adbde070aa
Abinav2907 <abinavm2907@gmail.com>
2026-06-13 18:39:58 +0530

    change5 for render

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import coreWebVitals from "eslint-config-next/core-web-vitals.js";
-import typescript from "eslint-config-next/typescript.js";
+import coreWebVitals from "eslint-config-next/core-web-vitals";
+import typescript from "eslint-config-next/typescript";
 
 export default defineConfig([
-  coreWebVitals,
-  typescript,
+  ...coreWebVitals,
+  ...typescript,
   globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
